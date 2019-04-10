eea1e9dc35522d1acee5c09ac2a45b6700536586
Eli Armstrong MACBOOKPRO <ELIJEFFREYARMSTRONG@GMAIL.COM>
2019-04-09 20:07:48 -0700

    added gif

--- MyHeroParking/AppDelegate.swift
+++ MyHeroParking/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         Parse.initialize(
             with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
-                
+           
             })
         )
         
